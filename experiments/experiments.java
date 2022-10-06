c8033a27750e07247db7c634aad073e0682210f1
Ben <ben.zhou.2006@gmail.com>
2022-10-06 11:23:06 -0400

    Push new experiments stuff

---
+++ experiments/experiments.java
@@ -0,0 +1,7 @@
+public class experiments {
+    public static void main(String[] args) {
+        String something = "something";
+        String somethingElse = "helloth";
+        System.out.println(something.compareTo(somethingElse));
+    }
+}
